566909f4211e733bfc4a599ab02d94a385b65bef
sarad.bajracharya <sarad_mi@yonefu.info>
2025-03-14 14:49:03 +0545

    [Changed] changed model and upgarded intl

--- example/android/app/src/main/kotlin/com/cleveroad/cr_calendar_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cleveroad.cr_calendar_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/src/models/calendar_event_model.dart
+++ lib/src/models/calendar_event_model.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
-final class CalendarEventModel {
+class CalendarEventModel {
   CalendarEventModel({
+    required this.id,
     required this.name,
     required this.begin,
     required this.end,
     this.eventColor = Colors.green,
+    this.imageUrl,
+    this.pdfUrl,
+    this.page,
+    this.allDay
   });
 
+  String id;
   String name;
   DateTime begin;
   DateTime end;
   Color eventColor;
+  String? imageUrl;
+  String? pdfUrl;
+  String? page;
+  bool? allDay;
 }
